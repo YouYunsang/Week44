d13c27852038dd3b4773abc349f0a94e195ca589
신상현 <sinsanghyeon1234@gmail.com>
2026-04-03 12:01:26 +0900

    IEvents

---
+++ Assets/01.Scripts/Events/GameEvents.cs
@@ -0,0 +1,24 @@
+// 슬라이스 성공
+public struct OnSliceEvent : IEvent
+{
+    public int score;
+    public int combo;
+}
+
+// 게임 오버
+public struct OnGameOverEvent : IEvent { }
+
+// 게임 시작
+public struct OnGameStartEvent : IEvent { }
+
+// 타임스케일 변경
+public struct OnTimeScaleChangedEvent : IEvent
+{
+    public float timeScale;
+}
+
+// 세팅 변경
+public struct OnSettingsChangedEvent : IEvent
+{
+    public SettingsData data;
+}
