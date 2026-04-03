ab5781dc8b53904a21b9636d397009d3ead36520
신상현 <sinsanghyeon1234@gmail.com>
2026-04-03 11:59:57 +0900

    inputsetting

---
+++ Assets/01.Scripts/Core/InputSO.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "InputSO", menuName = "Game/Input SO")]
+public class InputSO : ScriptableObject, NewInput.IPlayerActions
+{
+    public event Action<Vector2> OnMove;
+    public event Action<Vector2> OnLook;
+    public event Action          OnJump;
+    public event Action          OnAttack;
+    public event Action          OnCrouch;
+    public event Action          OnSprint;
+
+    NewInput _input;
+
+    void OnEnable()
+    {
+        _input = new NewInput();
+        _input.Player.AddCallbacks(this);
+        _input.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        _input.Player.RemoveCallbacks(this);
+        _input.Player.Disable();
+        if (Application.isPlaying)
+            _input.Dispose();
+    }
+
+    void NewInput.IPlayerActions.OnMove(InputAction.CallbackContext ctx)
+    {
+        OnMove?.Invoke(ctx.ReadValue<Vector2>());
+    }
+
+    void NewInput.IPlayerActions.OnLook(InputAction.CallbackContext ctx)
+    {
+        OnLook?.Invoke(ctx.ReadValue<Vector2>());
+    }
+
+    void NewInput.IPlayerActions.OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed) OnJump?.Invoke();
+    }
+
+    void NewInput.IPlayerActions.OnAttack(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed) OnAttack?.Invoke();
+    }
+
+    void NewInput.IPlayerActions.OnCrouch(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed) OnCrouch?.Invoke();
+    }
+
+    void NewInput.IPlayerActions.OnSprint(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed) OnSprint?.Invoke();
+    }
+
+    void NewInput.IPlayerActions.OnInteract(InputAction.CallbackContext ctx) { }
+    void NewInput.IPlayerActions.OnPrevious(InputAction.CallbackContext ctx) { }
+    void NewInput.IPlayerActions.OnNext(InputAction.CallbackContext ctx)     { }
+}
