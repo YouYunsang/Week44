33f2bd4ed2d3b98df36a42992a39c239307cd283
신상현 <sinsanghyeon1234@gmail.com>
2026-04-06 17:08:17 +0900

    떨어지는 속도 증가

--- Assets/01.Scripts/Player/SliceScripts/SliceFragment.cs
+++ Assets/01.Scripts/Player/SliceScripts/SliceFragment.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
 
-    /// <summary>
-    /// 슬라이스된 조각에 붙는 컴포넌트
-    /// 일정 시간 후 자동으로 제거됨
-    /// </summary>
+/// <summary>
+/// 슬라이스된 조각에 붙는 컴포넌트
+/// 일정 시간 후 자동으로 제거됨
+/// </summary>
 public class SliceFragment : MonoBehaviour
 {
     private const float DESTROY_DELAY = 6f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float _extraGravityMultiplier = 3f;
+
+    Rigidbody _rb;
+
     void Start()
     {
+        _rb = GetComponent<Rigidbody>();
         Destroy(gameObject, DESTROY_DELAY);
+    }
+
+    void FixedUpdate()
+    {
+        if (_rb != null)
+            _rb.AddForce(Physics.gravity * (_extraGravityMultiplier - 1f), ForceMode.Acceleration);
     }
 }
